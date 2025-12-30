9a9121114630b07b7c310de2170dec441e4b6815
Lapcuatui <macone-cw38zm@MacOne-CW38ZMs-MacBook-Pro.local>
2025-12-30 14:09:11 +0700

    section-hero-product

---
+++ js/main.js
@@ -0,0 +1,53 @@
+import { partnerLogos, productList, partnerLogoBasePath } from "./data.js";
+
+
+/* ================
+Nav
+================ */
+$(function (){
+  // show/hide nav
+  //$(".navbar").hideScroll();
+
+  //mobile nav toggle
+  const toggleBtn = $("#toggle_btn");
+  const dropdownMenu = $(".dropdown-menu");
+
+  toggleBtn.click(() => {
+    console.log("Hamburger icon clicked");
+    dropdownMenu.toggleClass("open")
+  })
+});
+
+/* ================
+PARTNER LOGOS
+================ */
+
+$(function () {
+  const container = document.getElementById("partner-logo-list");
+  partnerLogos.forEach((logo) => {
+    const img = document.createElement("img");
+    img.src = partnerLogoBasePath + logo.fileName;
+    img.alt = logo.alt;
+    img.classList.add("logo-ticket-image");
+    container.appendChild(img);
+  })
+})
+
+/* ================
+Products Tabs
+================ */
+
+$(function (){
+  // add active class to the first tab
+  //$('#product-tabs').find('li:first').addClass('activeTab');
+  $("li:first").addClass("activeTab");
+  // click change color of the first tab
+  $("li").on("click", function(){
+    $("li").removeClass("activeTab");
+    $('div[id="products-tabs"] ul .r-tabs-state-active').addClass("activeTab");
+  });
+
+  $("#products-tabs").responsiveTabs({
+   animation: 'slide'
+});
+})

fb3238058932de4650126c24c14a51c31fc67ae1
Lapcuatui <macone-cw38zm@MacOne-CW38ZMs-MacBook-Pro.local>
2026-01-03 22:07:25 +0700

    done-products-page

--- js/main.js
+++ js/main.js
@@ -90,3 +90,75 @@ $(function (){
   const el = document.querySelectorAll('.counter').forEach((node) => IO.observe(node))
   // IO.observe(el)
 })
+
+
+/* ================
+All Products
+================ */
+
+$(function (){
+  productList.map((product) => {
+    $("#product-item--container").append(`
+            <div data-filterable data-filter-category=${product.category}
+            class="relative col-span-3 overflow-hidden group hover:shadow-md">
+              <div class="portfolio-item">
+                <div>
+                  <img 
+                    src=${product.img}
+                    alt="product-img" />
+
+                    <div class="product-item-overlay">
+                      <div class="product-details">
+                        <h3>${product.name}</h3>
+                        <p>${product.description}</p>
+                      </div>
+                    </div>
+                </div>
+              </div>
+             </div>
+      `)
+  })
+  $.fn.filterjitsu();
+
+  // Active tab
+  function getAllUrlParams(url) {
+    url = url || window.location.href;
+    const param = {};
+
+    const queryString = url.split("?")[1];
+    if (!queryString) {
+      return param;
+    }
+
+    const [key, value] = queryString.split("=");
+    if(key) {
+      param[key] = value ? value : "";
+    }
+    return param;
+  }
+
+  const urlParam = getAllUrlParams();
+
+  $("#allProduct-filters a").removeClass("activeFilter")
+
+  const category = urlParam["filter-category"];
+
+  switch (category) {
+    case "whitetea":
+      $("#f-whitetea").addClass("activeFilter");
+      break;
+    case "blacktea":
+      $("#f-blacktea").addClass("activeFilter");
+      break;
+    case "oolong":
+      $("#f-oolong").addClass("activeFilter");
+      break;
+    case "matcha":
+      $("#f-matcha").addClass("activeFilter");
+      break;
+    default:
+      $("#f-all").addClass("activeFilter");
+      break;
+  }
+
+})
